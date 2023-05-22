8c5fcae707af521b5a475f30ef9c25e86b87901f
Alexander Willner <alex@willner.ws>
2023-05-22 22:04:46 +0200

    release 0.2.0

--- things_cli/__init__.py
+++ things_cli/__init__.py
@@ -1,10 +1,10 @@
 """A simple Python 3 CLI to read your Things app data."""
 
 __author__ = "Alexander Willner"
-__copyright__ = "2021 Alexander Willner"
+__copyright__ = "2023 Alexander Willner"
 __credits__ = ["Alexander Willner"]
 __license__ = "Apache License 2.0"
-__version__ = "0.1.3"
+__version__ = "0.2.0"
 __maintainer__ = "Alexander Willner"
 __email__ = "alex@willner.ws"
 __status__ = "Development"
